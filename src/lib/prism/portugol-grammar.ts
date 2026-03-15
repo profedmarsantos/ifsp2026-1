563fe154235f34d7a7c4e45c7bf0fb213dc3799c
[dyad] <profedmar.informatica@gmail.com>
2026-03-15 16:01:16 -0300

    Update 1 file

--- src/lib/prism/portugol-grammar.ts
+++ src/lib/prism/portugol-grammar.ts
@@ -1,64 +1,61 @@
 export const portugolPrism = {
+	comment: [
+		{
+			pattern: /\/\/.*/,
+			greedy: true
+		},
+		{
+			pattern: /\/\*[\s\S]*?\*\//,
+			greedy: true
+		}
+	],
 
-  comment: [
-    {
-      pattern: /\/\/.*/,
-      greedy: true
-    },
-    {
-      pattern: /\/\*[\s\S]*?\*\//,
-      greedy: true
-    }
-  ],
+	string: {
+		pattern: /(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,
+		greedy: true
+	},
 
-  string: {
-    pattern: /(["'])(?:\\.|(?!\1)[^\\\r\n])*\1/,
-    greedy: true
-  },
+	number: /\b\d+(\.\d+)?\b/,
 
-  number: /\b\d+(\.\d+)?\b/,
+	boolean: /\b(verdadeiro|falso)\b/,
 
-  boolean: /\b(verdadeiro|falso)\b/,
+	namespace: {
+		pattern: /\b(Matematica|Graficos|Arquivos|Sons|Teclado|Mouse|Calendario|Internet)(?=\.)/,
+		alias: 'class-name'
+	},
 
-  keyword: /\b(programa|funcao|escreva|leia|inicio|fim|se|senao|entao|enquanto|para|faca|retorne|pare|caso|escolha|const|inclua|biblioteca|inteiro|real|cadeia|caracter|logico)\b/,
+	'class-name': {
+		pattern: /\b(Matematica|Graficos|Arquivos|Sons|Teclado|Mouse|Calendario|Internet)\b/,
+		alias: 'builtin'
+	},
 
-  type: /\b(inteiro|real|cadeia|caracter|logico)\b/,
+	keyword: /\b(programa|funcao|escreva|leia|inicio|fim|se|senao|entao|enquanto|para|faca|retorne|pare|caso|escolha|const|inclua|biblioteca|inteiro|real|cadeia|caracter|logico)\b/,
 
-  builtin: /\b(limpa|pausa|sorteia|aleatorio)\b/,
+	builtin: /\b(limpa|pausa|sorteia|aleatorio)\b/,
 
-  operator: /\b(e|ou|nao)\b|==|!=|<=|>=|\+|-|\*|\/|%|<|>|=/,
+	operator: /\b(e|ou|nao)\b|==|!=|<=|>=|\+|-|\*|\/|%|<|>|=/,
 
-  'class-name': {
-    pattern: /\b(Matematica|Graficos|Arquivos|Sons|Teclado|Mouse|Calendario|Internet)\b/,
-    alias: 'builtin'
-  },
+	constant: /\b(PI|VERDADEIRO|FALSO)\b/,
 
-  namespace: {
-    pattern: /\b(Matematica|Graficos|Arquivos|Sons|Teclado|Mouse|Calendario|Internet)(?=\.)/,
-    alias: 'class-name'
-  },
+	function: {
+		pattern: /\b[a-zA-Z_]\w*(?=\s*\()/,
+		greedy: true
+	},
 
-  function: {
-    pattern: /\b[a-zA-Z_]\w*(?=\s*\()/,
-    greedy: true
-  },
+	array: {
+		pattern: /\[[^\]]*\]/,
+		inside: {
+			number: /\b\d+\b/,
+			punctuation: /[\[\],]/
+		}
+	},
 
-  property: {
-    pattern: /(?<=\.)[a-zA-Z_]\w*/,
-    alias: 'function'
-  },
+	property: {
+		pattern: /(?<=\.)[a-zA-Z_]\w*/,
+		alias: 'function'
+	},
 
-  constant: /\b(PI|VERDADEIRO|FALSO)\b/,
+	identifier: /\b[a-zA-Z_]\w*\b/,
 
-  array: {
-    pattern: /\[[^\]]*\]/,
-    inside: {
-      number: /\b\d+\b/,
-      punctuation: /[\[\],]/
-    }
-  },
-
-  identifier: /\b[a-zA-Z_]\w*\b/,
-
-  punctuation: /[{}[\];(),.:]/
-}
+	punctuation: /[{}[\];(),.:]/
+};

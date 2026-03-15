e36da1c221c398635e4f626f8b56ab1f818b35a1
[dyad] <profedmar.informatica@gmail.com>
2026-03-15 16:54:02 -0300

    Updated src/components/MarkdownCodeBlock.tsx

--- src/components/MarkdownCodeBlock.tsx
+++ src/components/MarkdownCodeBlock.tsx
@@ -8,14 +8,14 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 
 // Importe os componentes de linguagem do Prism que você espera usar.
 // Adicione mais conforme necessário para outras linguagens.
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-markup'; // Para HTML/XML
-import 'prismjs/components/prism-bash'; // Para scripts de shell
-import 'prismjs/components/prism-json'; // Para JSON
+// import 'prismjs/components/prism-javascript';
+// import 'prismjs/components/prism-typescript';
+// import 'prismjs/components/prism-css';
+// import 'prismjs/components/prism-markup'; // Para HTML/XML
+// import 'prismjs/components/prism-bash'; // Para scripts de shell
+// import 'prismjs/components/prism-json'; // Para JSON
 import 'prismjs/components/prism-python'; // Exemplo: para Python
-import 'prismjs/components/prism-java'; // Exemplo: para Java
+// import 'prismjs/components/prism-java'; // Exemplo: para Java
 import 'prismjs/components/prism-c'; // Adicionado: Para a linguagem C
 
 interface MarkdownCodeBlockProps {

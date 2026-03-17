8c6ac5dc0821af0c91354bff05a1b17993b638e9
[dyad] <profedmar.informatica@gmail.com>
2026-03-17 17:22:07 -0300

    [dyad] Exibindo o tutorial de operadores lógicos e aritméticos na página inicial - wrote 2 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,15 +1,16 @@
 import { MadeWithDyad } from "@/components/made-with-dyad";
 import MarkdownRenderer from "@/components/MarkdownRenderer";
-import PrintButton from "@/components/PrintButton"; // Importar o novo componente
+import PrintButton from "@/components/PrintButton";
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center bg-background p-8 relative"> {/* Adicionado 'relative' para posicionamento absoluto */}
-      <div className="absolute top-8 right-8 z-10"> {/* Posiciona o botão no canto superior direito */}
+    <div className="min-h-screen flex flex-col items-center bg-background p-8 relative">
+      <div className="absolute top-8 right-8 z-10">
         <PrintButton />
       </div>
-      <div className="max-w-4xl w-full mt-16"> {/* Adicionado 'mt-16' para dar espaço ao botão */}
-        <MarkdownRenderer markdownPath="/README.md" /> {/* Alterado o caminho para README.md */}
+      <div className="max-w-4xl w-full mt-16">
+        {/* Alterado o caminho para o novo arquivo de conteúdo */}
+        <MarkdownRenderer markdownPath="/src/content/Operadores logicos e aritmeticos.md" />
       </div>
       <MadeWithDyad />
     </div>
